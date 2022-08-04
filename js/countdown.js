4f57a5ebff2791c7e81e319b63095bd77da250c9
Justin Duong <jduong@umich.edu>
2022-08-04 18:42:15 -0400

    Added css, worked on css flip animation.

--- js/countdown.js
+++ js/countdown.js
@@ -5,9 +5,11 @@
   14 days 0 Hours 0 Minutes 0 Seconds
   13 days 23 Hours 59 Minutes 59 59 Seconds
 
+  // Every second, we need to run update function on the flip cards
+  // Update function
+  // 
 
 */
-
 document.addEventListener('DOMContentLoaded', () => {
  
   const dayMarker = document.getElementById('day-card');
@@ -20,7 +22,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const exactDay = dayMarker.children[0].innerHTML;
     const exactHour = hourMarker.children[0].innerHTML;
     const exactMinute = minuteMarker.children[0].innerHTML;
-    const exactSecond = secondMarker.children[0].innerHTML;
+    const exactSecond = secondMarker.children[0].children[0].innerHTML;
+
+    
+    const top = document.querySelector('.card-top');
+    const bottom = document.querySelector('.card-bottom');
+    const back = document.querySelector('.card-back');
+    const backBottom = document.querySelector('.card-back .card-bottom');
+    console.log(top, bottom, back, backBottom);
 
     // Time left in milliseconds=
     const distanceFromDate = fourteenDaysFromNow - now;
@@ -33,15 +42,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     var minutes = Math.floor((distanceFromDate % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distanceFromDate % (1000 * 60)) / 1000);
-    minuteMarker.children[0].innerHTML = minutes;
-    secondMarker.children[0].innerHTML = seconds;
+    minuteMarker.children[0].children[0].innerHTML = minutes;
+    secondMarker.children[0].children[0].innerHTML = seconds;
     // Add class in order to create animation.
 
+    /*
     if (exactSecond != seconds) {
+      if (exactSecond > 0) {
+        back.innerHTML = exactSecond;
+        bottom.innerHTML = exactSecond;
+      }
+      top.innerHTML = exactSecond;
+      backBottom.innerHTML = exactSecond;
+
       secondMarker.classList.add('flip');
+      exactSecond = exactSecond - 1;
       setTimeout(() => {
         secondMarker.classList.remove('flip');
       }, 500);
     }
+    */
   }, 1000);
 });
